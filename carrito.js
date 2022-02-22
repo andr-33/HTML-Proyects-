798fc6b6cc48829c6b6abc91fa19fbe58b513087
Andree Serrano <andree.serranourraca@gmail.com>
2022-02-22 10:23:55 +0100

    Update 'carrito.js' with the products list and the sum

---
+++ carrito.js
@@ -0,0 +1,51 @@
+$(document).ready(async function() {
+    // JSON CON DATOS DEL DOCUMENTO 
+    const documento = await $.getJSON("./database.json");
+    let carrito = localStorage.getItem("productos");
+    carrito = JSON.parse(carrito);
+
+    let html = '';
+    let Artuculos = '';
+    let totalView = '';
+    let total = 0;
+    let NuArticulos = carrito.length;
+    
+
+    documento.forEach(element => {
+        // ITINERAR LOS DATOS DEL DOCUMENTO AQUI TENES QUE PONER UNA DOBLE ITINERACION Y UN IF PARA IMPRIMIR O NO LOS DATOS 
+        carrito.forEach(carrito => {
+            if (carrito == element.id) {
+                // IMPRIMIR
+                html += `
+                <li class="list-group-item d-flex justify-content-between lh-sm">
+                        <div>
+                            <h6 class="my-0">${element.title}</h6>
+                        </div>
+                        <span class="text-muted">€${element.price}</span>
+                </li>
+                `;
+
+                total += parseFloat(element.price);
+            }
+
+            Artuculos = `
+            <span class="text-secondary">Tú Carrito</span>
+            <span class="badge bg-primary rounded-pill">${NuArticulos} prendas</span>`;
+
+            totalView = `
+            <li class="list-group-item d-flex justify-content-between">
+                <span>Total </span>
+                <strong>€ ${total}</strong>
+            </li>`;
+
+        })
+
+    });
+    
+    $("#cont").html(html);
+    $("#numArticulos").html(Artuculos);
+    $("#total").html(totalView);
+    console.log(total);
+
+    // Tambien hace que el precio del total coincida
+});
---
+++ index.js
@@ -0,0 +1,49 @@
+$(document).ready(async function() {
+    const documento = await $.getJSON("./database.json");
+
+    console.log(documento);
+    let html = '';
+    documento.forEach(producto => { 
+        html += ` 
+        <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="${producto.image}" class="card-img-top" style ="max-width:150px; max-heigth;">
+            <div class="card-body">
+                <!--Texto para descripción de la prenda-->
+                <p class="card-text">${producto.title}</p>
+                <!--Texto del precio de la prenda-->
+                <small class="text-muted">€ ${producto.price}</small>
+                <div class="d-flex justify-content-between align-items-center">
+                    <!--Boton para agregar al carrito de compras-->
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary btnAddProduct" data-id="${producto.id}">Agregar</button>
+                    </div>
+                    <p>Rating: ${producto.rating.rate}</p>
+                </div>
+            </div>
+        </div>
+    </div>`;
+    });
+
+    $("#contenedor").html(html);
+
+
+    $(".btnAddProduct").on("click", function() {
+        const { id } = $(this).data();
+        const carrito = localStorage.getItem("productos");
+
+        console.log(carrito);
+        if (carrito) {
+            let data = JSON.parse(carrito);
+            data.push(id);
+            data = JSON.stringify(data);
+            localStorage.setItem("productos", data);
+        } else {
+            let data = [id];
+            data = JSON.stringify(data);
+            localStorage.setItem("productos", data);
+        }
+        alert("Producto agregado al carrito")
+
+    });
+});
